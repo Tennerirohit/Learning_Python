4498961500beb7ae9e33d712a4706758e9f82f01
Tenneri Rohit <134133259+Tennerirohit@users.noreply.github.com>
2025-12-28 13:28:23 +0530

    Comparision Operators

---
+++ Comparision_operators.py
@@ -0,0 +1,9 @@
+''' Comparision operators are used to compare two variables'''
+a= int(input("Enter the first value"))
+b= int(input("Enter the Second value"))
+print(f"Is {a} Equals to  {b} : {a==b}")
+print(f"Is {a} Not Equals to  {b} : {a!=b}")
+print(f"Is {a} Greater than {b} : {a>b}")
+print(f"Is {a} Less than {b} : {a<b}")
+print(f"Is {a} Greater than or Equals to {b} : {a>=b}")
+print(f"Is {a} Less than or Equals to {b} : {a<=b}")
