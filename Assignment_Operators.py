9b745795e0c2ff3aef8d733ebda09a3dd620cd4e
Tennerirohit <tennerirohit@gmail.com>
2025-12-28 13:11:35 +0530

    Assignment operator

---
+++ Assignment_Operators.py
@@ -0,0 +1,25 @@
+'''
+= Assignment : Assigns the value on the right to the variable on the left 
++= Add and assign : add the right opperand to the variable on the left and assigns the result to the variable
+-= Subtract and assign : subtract the right opperand to the variable on the left and assigns the result to the variable
+*= Multiply and assign : subtract the right opperand to the variable on the left and assigns the result to the variable
+/= Divide and assign : subtract the right opperand to the variable on the left and assigns the result to the variable
+//= Floor divide and assign : subtract the right opperand to the variable on the left and assigns the result to the variable
+
+'''
+
+a= 20
+b= 10
+c=2
+d=3
+e=10
+a+=b  #The value of a and b are saved in the a 
+b-=a  
+c*=d
+d/=c
+e//=c
+print(a) 
+print(b) 
+print(c) 
+print(d) 
+print(e) 
