cfe5f475de98c8a146518a6b7285c3ee046460ee
Tennerirohit <tennerirohit@gmail.com>
2025-12-28 12:51:34 +0530

    Arithmetic Operator

---
+++ Arithmetic_Operators.py
@@ -0,0 +1,13 @@
+'''Arithematic operators are used to solve the arithmetic problems
+   +,-,/,%,*,//,** 
+   '''
+
+a= int(input("Enter the first value"))
+b= int(input("Enter the Second value"))
+print(f"The addition of {a} and {b} is a+b : {a+b}")
+print(f"The Subtraction of {a} and {b} is a-b : {a-b}")
+print(f"The Division of {a} and {b} is a/b : {a/b}")
+print(f"The Multiplication of {a} and {b} is a*b : {a*b}")
+print(f"The Floor division of {a} and {b} is a//b : {a//b}")
+print(f"The modular division of {a} and {b} is a%b: {a%b}")
+print(f"The Exponent  of {a} and {b} is a**b: {a**b}")
