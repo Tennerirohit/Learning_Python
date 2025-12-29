d2f3a6bfbb53f92c99f3ae70e685463e337d1937
Tennerirohit <tennerirohit@gmail.com>
2025-12-29 12:03:30 +0530

    Logical Operators

---
+++ Logical_Operators.py
@@ -0,0 +1,7 @@
+'''Logical operators are ued to logically compare two variables '''
+
+a= True
+b= False
+print(f"Is {a} Equals to  {b} : {a and b}")
+print(f"Reversing value of {b} : {not b}")
+print(f"Is {a} OR  {b} : {a or b}")
