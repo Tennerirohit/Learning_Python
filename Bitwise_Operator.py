10b131bfa993a7c87dabcc0bafa2339060191ad5
Tennerirohit <tennerirohit@gmail.com>
2025-12-29 12:48:55 +0530

    Example of bitwise operator

---
+++ Bitwise_Operator.py
@@ -0,0 +1,14 @@
+'''Bitwise AND(&) :- Sets each bit to 1 if both bits are 1
+   Bitwise OR (|) :- Sets each bit to 1 atleast one of the bits is 1
+   Bitwise XOR(^) :- Sets each bit to 1 only if one of the bit is 1
+   Bitwise NOT(~) :- Inverts the bits changing 1 to 0
+   Bitwise Left Shift(<<) :- Shifts the bits to the left by specifed number of positions 
+   Bitwise Right Shift(>>) :- Shifts the bits to the Right by specifed number of positions '''
+a=2
+b=3
+print(a&b)
+print(a|b)
+print(a^b)
+print(~a)
+print(2>>1)
+print(2<<1)
