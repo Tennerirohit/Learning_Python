7c5405853ee41a4cc68096cb1d60649982115d35
Tennerirohit <tennerirohit@gmail.com>
2025-12-29 13:06:02 +0530

    Quadratic equation

---
+++ Quadratic_Equation.py
@@ -0,0 +1,6 @@
+a=int(input("Enter the value of a :- "))
+b=int(input("Enter the value of b :- "))
+c=int(input("Enter the value of c :- "))
+d=b**2-4*a*c
+print((-b+d**-2)/2*a)
+print((-b-d**-2)/2*a)
