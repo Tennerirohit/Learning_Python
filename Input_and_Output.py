9310e62563e371b85a2b27f139641f467a2809a2
Tennerirohit <tennerirohit@gmail.com>
2025-12-29 12:58:54 +0530

    Input and output

---
+++ Input_and_Output.py
@@ -0,0 +1,6 @@
+''' Input:- The input() function is used to take input from the user via keyboard during the program's execution
+    Output:- The print statement is used to display output to the console or the terminal. It allows you to show information'''
+
+a=input("Enter the input : ") #Taking the input value
+print(a) #Printing the input that we have given 
+
