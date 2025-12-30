bb50e1dde5052d41ab4d267cd470972e8d6f5fd8
Tennerirohit <tennerirohit@gmail.com>
2025-12-30 18:20:14 +0530

    Calculator using conditional statements

--- Calculator.py
+++ Calculator.py
@@ -1,2 +1,15 @@
-a= int(input("Enter the digit : "))
-b= int(input("Enter the digit : "))
+a= float(input("Enter the digit : "))
+b= float(input("Enter the digit : "))
+operator= input("Enter the Operator : ")
+if operator == "+" :
+    print("The addition value : ",a+b)
+elif operator =="-":
+    print("The subtracted value : ",a-b)
+elif operator =="/":
+    print("The Divided value : ",a/b)
+elif operator =="-":
+    print("The Multiplied value : ",a*b)
+elif operator =="-":
+    print("The Modular division value : ",a%b)
+else :
+    print("Invalid operator ,Enter the operator again")
