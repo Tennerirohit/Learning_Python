a1d6d6a7cbb3002c73683ca8f6dd3ab3bf57c107
Tennerirohit <tennerirohit@gmail.com>
2026-01-04 10:54:12 +0530

    Sum of two numbers using functions

---
+++ Sum_Of_Two_Numbers.py
@@ -0,0 +1,13 @@
+#Problems on functions
+
+#1. Finding the sums of two Numbers 
+#sample input :- num1=5,num2=10
+#sample Output :- sum=15
+
+num1=int(input("Enter the number : "))
+num2=int(input("Enter the number : "))
+def sum(num1,num2):
+    r=num1+num2
+    return (r)
+p= sum(num1,num2)
+print(p)
